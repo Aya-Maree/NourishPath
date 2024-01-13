b8bd308a2d889f5f286db33c7bd93b9035c3f49c
aya maree <ayamaree77@gmail.com>
2024-01-12 23:25:18 -0500

    created pages

--- client/nourish-path/src/App.js
+++ client/nourish-path/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+// Import your page components
+import HomePage from './pages/homepage';
+import ChatBot from './pages/chatBot';
+import Recipes from './pages/recipes';
+import Journal from './pages/journals';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* Navigation Links */}
+        <nav>
+          <Link to="/">Home</Link> | 
+          <Link to="/chatbot">Chatbot</Link> | 
+          <Link to="/recipes">Recipes</Link> | 
+          <Link to="/journaling">Journaling</Link>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<HomePage />} exact />
+          <Route path="/chatbot" element={<ChatBot />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/journaling" element={<Journal />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/nourish-path/src/pages/recipes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Recipes() {
+  return (
+    <div>
+      <h1>Recipe</h1>
+    </div>
+  );
+}
+
+export default Recipes;
