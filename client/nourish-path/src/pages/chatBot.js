cb2e817967306254b3b6d10d5b69b3bb3ed71caf
Lara El-Aloul <lelalou@uwo.ca>
2024-01-13 21:50:47 -0500

    add openai

--- client/nourish-path/src/pages/chatBot.js
+++ client/nourish-path/src/pages/chatBot.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import OpenAI from 'openai'
+
+const openai = new OpenAI({
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true
+})
 
 function ChatBot(){
+    const [questionType, setQuestionType] = useState('general')
+
     return(
-        <div>
-            <h1>ChatBot</h1>
-        </div>
+       <Container className='mt-3'>
+        hi {process.env.REACT_APP_OPENAI_API_KEY.slice(0,5)}
+       </Container>
     );
 }
 export default ChatBot;
